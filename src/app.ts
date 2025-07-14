87942bd080d966583694b6755fc5f9a844111087
rayhan2392 <rkshawnso@gmail.com>
2025-07-14 23:59:04 +0600

    cors issue solved

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { Application, Request, Response } from "express"
 import { booksRouter } from "./controllers/book.controllers";
 import { borrowBookRouter } from "./controllers/borrowBook.controllers";
+import cors from "cors";
 
 const app: Application = express();
 app.use(express.json())
+app.use(cors())
 
 app.use("/api/books",booksRouter);
 app.use("/api/borrow",borrowBookRouter)

6c555043205e73755da4f9a0ecee9044b9d29d98
rayhan2392 <rkshawnso@gmail.com>
2025-07-16 21:50:52 +0600

    msg

--- src/app.ts
+++ src/app.ts
@@ -3,16 +3,28 @@ import { booksRouter } from "./controllers/book.controllers";
 import { borrowBookRouter } from "./controllers/borrowBook.controllers";
 import cors from "cors";
 
-const app: Application = express();
+const app = express();
 app.use(express.json());
 
 
 
 
-app.use(cors({
-  origin: ['http://localhost:5173', 'https://library-management-frontend-chi.vercel.app']
+// app.use(cors({
+//   origin: ['http://localhost:5173', 'https://library-management-frontend-chi.vercel.app'],
+//   method: 'GET, HEAD, PUT, PATCH, POST, DELETE, OPTIONS',
+// “preflightContinue”: false,
+// “optionsSuccessStatus”: 200,
+// “credentials”: true
 
-}))
+// }))
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://library-management-frontend-chi.vercel.app'],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+  credentials: true 
+}));
 
 app.use("/api/books", booksRouter);
 app.use("/api/borrow", borrowBookRouter);
